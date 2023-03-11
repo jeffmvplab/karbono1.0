1440fc2cd36bd8243dd60c8768787abee42f2af3
danner01 <92951539+danner01@users.noreply.github.com>
2023-03-11 14:36:26 -0500

    register

--- src/context/GlobalContext.ts
+++ src/context/GlobalContext.ts
@@ -2,25 +2,41 @@ import { createContext } from "react";
 
 
 interface ContextProps {
-    
-    authOK:boolean;
 
-    login:()=>void;
-    logout:()=>void;
-    isAuth:boolean;
-    
-    loadingAuth:boolean,
-    email:string,
-    errorEmail:boolean,
-    messageErrorEmail:string,
-    handleEmail:(event: React.ChangeEvent<HTMLInputElement>)=>void,
+    authOK: boolean;
 
-    password:string,
-    errorPassword:boolean,
-    messageErrorPassword:string,
-    handlePassword:(event: React.ChangeEvent<HTMLInputElement>)=>void,
+    login: () => void;
+    register:()=>void;
+    logout: () => void;
+    isAuth: boolean;
+
+    loadingAuth: boolean,
+
+    name: string,
+    errorName: boolean,
+    messageErrorName: string,
+    handleName: (event: React.ChangeEvent<HTMLInputElement>) => void,
+    
+    phone: string,
+    errorPhone: boolean,
+    messageErrorPhone: string,
+    handlePhone: (event: React.ChangeEvent<HTMLInputElement>) => void,
+
+    email: string,
+    errorEmail: boolean,
+    messageErrorEmail: string,
+    handleEmail: (event: React.ChangeEvent<HTMLInputElement>) => void,
+
+    password: string,
+    errorPassword: boolean,
+    messageErrorPassword: string,
+    handlePassword: (event: React.ChangeEvent<HTMLInputElement>) => void,
+
+    passwordConfirm: string,
+    errorPasswordConfirm: boolean,
+    messageErrorPasswordConfirm: string,
+    handlePasswordConfirm: (event: React.ChangeEvent<HTMLInputElement>) => void,
 
 }
 
 export const GlobalContext = createContext({} as ContextProps)
-   
