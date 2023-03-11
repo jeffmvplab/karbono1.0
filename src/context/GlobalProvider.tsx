94067b5c28c05b64724bfd148e7e6e8a1b33ce02
Anderson Portilla <124816381+Andersonportilla0206@users.noreply.github.com>
2023-03-11 15:14:08 -0500

    commit 12

--- src/context/GlobalProvider.tsx
+++ src/context/GlobalProvider.tsx
@@ -4,7 +4,7 @@ import { LocalStorageProtocol } from "@/protocols/cache/local_cache";
 import { mainRoutes } from "@/routes/routes";
 import { StorageKeysEnum } from "@/utilities/enums";
 import { useRouter } from "next/router";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { FC } from "react";
 import { GlobalContext } from "./GlobalContext";
 
@@ -114,7 +114,17 @@ export const GlobalProvider: FC<Props> = ({ children }) => {
 
 	}, [])
 
+	///////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////
 
+
+	const [ruta, setRuta ] =  useState(' ');
+
+
+
+	const cambiarRuta = () => {
+		setRuta (mainRoutes.prescripcion);
+	}
 
 	
 	return (
@@ -137,8 +147,8 @@ export const GlobalProvider: FC<Props> = ({ children }) => {
 			messageErrorPassword,
 			handlePassword,
 
-			// ruta,
-			// cambiarRuta
+			ruta,
+			cambiarRuta
 
 		}}>{children}
 		</GlobalContext.Provider>
