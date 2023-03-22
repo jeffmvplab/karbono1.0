9e6c04f3d680212d4ba0d153404f5e1619737021
danner01 <92951539+danner01@users.noreply.github.com>
2023-03-22 19:43:11 -0400

    responsive form

--- src/views/Formulario/FormView.tsx
+++ src/views/Formulario/FormView.tsx
@@ -18,48 +18,61 @@ const FormView: React.FC<FormViewProps> = () => {
 				Nueva Orden
 			</Typography>
 
-			<Grid container direction='column' xs={12} sm={8} md={9} style={{ padding:6 }}>
+			<Grid container>
 
-				<Grid container spacing={2}>
+				<Grid container direction='column' xs={12} sm={8} md={9} style={{ padding: 6 }}>
 
-					<Grid xs={12} sm={6} md={4} style={{padding:'10px'}} >
-						<TextField
-							id='Numero-de-orden'
-							label='Número de Orden'
-							type='text'
-							variant='outlined'
-							color='secondary'
-							fullWidth
-						/>
-					</Grid>
-					<Grid xs={12} sm={6} md={4} style={{padding:'10px'}} >
-						<TextField
-							id='Tipo-de-prescripción'
-							label='Tipo de prescripción'
-							type='text'
-							variant='outlined'
-							color='secondary'
-							fullWidth
-						/>
-					</Grid>
-					<Grid xs={12} sm={6} md={4} style={{padding:'10px'}} >
-						<TextField
-							id='Fecha-de-creación'
-							label='Fecha de creación'
-							type='text'
-							variant='outlined'
-							color='secondary'
-							fullWidth
-						/>
+					<Grid container spacing={2}>
+
+						<Grid xs={12} sm={6} md={4} style={{ padding: '10px' }} >
+							<TextField
+								id='Numero-de-orden'
+								label='Número de Orden'
+								type='text'
+								variant='outlined'
+								color='secondary'
+								fullWidth
+							/>
+						</Grid>
+						<Grid xs={12} sm={6} md={4} style={{ padding: '10px' }} >
+							<TextField
+								id='Tipo-de-prescripción'
+								label='Tipo de prescripción'
+								type='text'
+								variant='outlined'
+								color='secondary'
+								fullWidth
+							/>
+						</Grid>
+						<Grid xs={12} sm={6} md={4} style={{ padding: '10px' }} >
+							<TextField
+								id='Fecha-de-creación'
+								label='Fecha de creación'
+								type='text'
+								variant='outlined'
+								color='secondary'
+								fullWidth
+							/>
+						</Grid>
+
 					</Grid>
 
-				</Grid>
-		
 					<InformacionPaciente />
 					<Macronutrientes />
 					<Micronutrientes />
 
-		
+
+
+				</Grid>
+
+
+				<Grid 
+				container 
+				direction='column'  
+				sm={4} md={3} 
+				style={{padding:6,backgroundColor:'aquamarine'}}>
+
+				</Grid>
 
 			</Grid>
 
