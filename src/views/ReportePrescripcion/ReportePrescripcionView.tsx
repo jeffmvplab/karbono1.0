694f7369bc36a5c82ed004c6622a585e1511a626
danner01 <92951539+danner01@users.noreply.github.com>
2023-04-28 15:17:35 -0400

    www

--- src/views/ReportePrescripcion/ReportePrescripcionView.tsx
+++ src/views/ReportePrescripcion/ReportePrescripcionView.tsx
@@ -18,35 +18,35 @@ import { colorsKarbono } from '@/themes/colors';
 export interface ReportePrescripcionViewProps { }
 
 const ReportePrescripcionView: React.FC<ReportePrescripcionViewProps> = () => {
-  
-	
-	const { getPrescriptionsByNumber,handleOpenModalDescargar,reporte} = useContext(ReportesContext)
-    
+
+
+	const { getPrescriptionsByNumber, handleOpenModalDescargar, reporte } = useContext(ReportesContext)
+
 	useEffect(() => {
 		getPrescriptionsByNumber();
-	  }, [])
-	 
+	}, [])
+
 
 	return (
 		<>
-		<DescargarModal/>
-		<OrdenarModal/>
+			<DescargarModal />
+			<OrdenarModal />
 			<Grid container sx={{ marginTop: '100px', paddingRight: '25px', paddingLeft: '25px', marginBottom: '30px' }}>
 				<Grid item display='flex' justifyContent='space-between' width='100%'>
 
 					<Typography variant='h5' sx={{ fontWeight: 700 }}>Reporte prescripción</Typography>
 
-					<CustomButton text={'Descargar'}	
-					onClick={handleOpenModalDescargar}
-					 width='180px'
-					 height='44px'
-					 variant='outlined'
-					 color='secundary'
-					fontSize={'16px'}
-					 textColor={colorsKarbono.secundary}
-					 borderColor={colorsKarbono.secundary}
-					 endIcon={<GetAppOutlinedIcon style={{ color: '#372fc6', paddingLeft: '5px',scale:'1.5' }} />}
-					 sx={{borderRadius:'10px'}}
+					<CustomButton text={'Descargar'}
+						onClick={handleOpenModalDescargar}
+						width='180px'
+						height='44px'
+						variant='outlined'
+						color='secundary'
+						fontSize={'16px'}
+						textColor={colorsKarbono.secundary}
+						borderColor={colorsKarbono.secundary}
+						endIcon={<GetAppOutlinedIcon style={{ color: '#372fc6', paddingLeft: '5px', scale: '1.5' }} />}
+						sx={{ borderRadius: '10px' }}
 					/>
 
 					{/* <Link href='' style={{ textDecoration: 'none' }}>
@@ -62,10 +62,10 @@ const ReportePrescripcionView: React.FC<ReportePrescripcionViewProps> = () => {
 				</Grid>
 
 				<Grid item display={'flex'} sx={{ width: '100%', marginTop: '30px', marginRight: '5px', backgroundColor: '#F0F0F0', height: '50px', alignItems: 'center', borderRadius: '10px 10px 0 0' }}>
-					
+
 					<Typography sx={{ color: '#372FC6', fontWeight: 600, fontSize: '13px', paddingLeft: '10px', width: '20%', textAlign: 'center' }}>IPS:{reporte?.ips}</Typography>
 					<Divider orientation='vertical' style={{ height: '60%' }} />
-					
+
 					<Typography sx={{ color: '#372FC6', fontWeight: 600, fontSize: '13px', paddingLeft: '10px', width: '20%', textAlign: 'center' }}>Servicio:{reporte?.servicio}</Typography>
 					<Divider orientation='vertical' style={{ height: '60%' }} />
 
@@ -75,27 +75,27 @@ const ReportePrescripcionView: React.FC<ReportePrescripcionViewProps> = () => {
 					<Divider orientation='vertical' style={{ height: '60%' }} /> */}
 					<Typography sx={{ color: '#372FC6', fontWeight: 600, fontSize: '15px', paddingLeft: '10px', width: '20%', textAlign: 'center' }}>Peso:{reporte?.peso}</Typography>
 					<Divider orientation='vertical' style={{ height: '60%' }} />
-					
+
 					<Typography sx={{ color: '#372FC6', fontWeight: 600, fontSize: '13px', paddingLeft: '10px', width: '20%', textAlign: 'center' }}>Identificación:{reporte?.no_identificacion}</Typography>
 				</Grid>
 
-				<Grid item display={'flex'} sx={{ width: '100%', marginTop: '0px', marginRight: '5px', backgroundColor: '#F0F0F0', height: '50px', alignItems: 'center', borderRadius: '0px 0px 10px 10px',  }}>
-					
+				<Grid item display={'flex'} sx={{ width: '100%', marginTop: '0px', marginRight: '5px', backgroundColor: '#F0F0F0', height: '50px', alignItems: 'center', borderRadius: '0px 0px 10px 10px', }}>
+
 					<Typography sx={{ color: '#372FC6', fontWeight: 600, fontSize: '13px', paddingLeft: '10px', width: '20%', textAlign: 'center' }}>Infusión continua por (hr):{reporte?.tiempo_infusion}</Typography>
 					<Divider orientation='vertical' style={{ height: '60%' }} />
-					
+
 					<Typography sx={{ color: '#372FC6', fontWeight: 600, fontSize: '13px', paddingLeft: '10px', width: '20%', textAlign: 'center' }}>Volumen de purga:{reporte?.purga}</Typography>
 					<Divider orientation='vertical' style={{ height: '60%' }} />
-					
+
 					<Typography sx={{ color: '#372FC6', fontWeight: 600, fontSize: '13px', paddingLeft: '10px', width: '20%', textAlign: 'center' }}>Vía de administración:{reporte?.via_administracion}</Typography>
 					<Divider orientation='vertical' style={{ height: '60%' }} />
-					
-					<Typography sx={{ color: '#372FC6', fontWeight: 600, fontSize: '13px', paddingLeft: '10px', width: '20%', textAlign: 'center' }}>Volumen total NPT + purga:{(reporte?.volumen!+reporte?.purga!)}</Typography>
+
+					<Typography sx={{ color: '#372FC6', fontWeight: 600, fontSize: '13px', paddingLeft: '10px', width: '20%', textAlign: 'center' }}>Volumen total NPT + purga:{(reporte?.volumen! + reporte?.purga!)}</Typography>
 					<Divider orientation='vertical' style={{ height: '60%' }} />
-					
+
 					<Typography sx={{ color: '#372FC6', fontWeight: 600, fontSize: '13px', paddingLeft: '10px', width: '20%', textAlign: 'center' }}>Volumen total NPT:{reporte?.volumen}</Typography>
 					<Divider orientation='vertical' style={{ height: '60%' }} />
-					
+
 					<Typography sx={{ color: '#372FC6', fontWeight: 600, fontSize: '13px', paddingLeft: '10px', width: '20%', textAlign: 'center' }}>Flujo metabólico:{reporte?.flujo_metabolico}</Typography>
 				</Grid>
 				<ReportesMicronutrientes />
@@ -105,10 +105,9 @@ const ReportePrescripcionView: React.FC<ReportePrescripcionViewProps> = () => {
 				<Divider orientation='horizontal' style={{ width: '50%' }} />
 
 				<ReportesParametros />
-				<Divider orientation='horizontal' style={{ width: '50%'}} />
+				<Divider orientation='horizontal' style={{ width: '50%' }} />
 
 				<BannerOrdenar />
-				
 			</Grid>
 
 		</>
