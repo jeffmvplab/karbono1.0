cfce1dc8d4e92c7f774e9d8662da598e00cb21a3
danner01 <92951539+danner01@users.noreply.github.com>
2023-05-22 20:33:45 -0400

    rextricciones

--- src/themes/light-themes.ts
+++ src/themes/light-themes.ts
@@ -29,6 +29,33 @@ export const lightThemes = createTheme({
 
             }
         },
+        MuiTypography: {
+            defaultProps: {
+                fontFamily:"Outfit,sans-serif",
+            //   variantMapping: {
+            //     h1: 'h1',
+            //     h2: 'h2',
+            //     h3: 'div',
+            //     h4: 'div',
+            //     h5: 'div',
+            //     h6: 'div',
+            //     subtitle1: 'div',
+            //     subtitle2: 'div',
+            //     body1: 'div',
+            //     body2: 'div'
+            //   }
+            // },
+            // styleOverrides: {
+            //   gutterBottom: {
+            //     marginBottom: 4
+            //   },
+            //   paragraph: {
+            //     fontSize: 17,
+            //     lineHeight: 1.7
+            //   }
+            }
+          },
+
         MuiTextField: {
             defaultProps: {},
             styleOverrides: {
