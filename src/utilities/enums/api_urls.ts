94534470c048820fb0db1bd7940e8d4da24ba3e0
danner01 <92951539+danner01@users.noreply.github.com>
2023-05-24 17:32:16 -0400

    aa

--- src/utilities/enums/api_urls.ts
+++ src/utilities/enums/api_urls.ts
@@ -4,7 +4,7 @@ export enum ApiUrlsEnum {
     login = 'auth/login',
     savePrescriptions='prescriptions',
     getAllPrescriptions='prescriptions/all',
-    getPrescriptionsByNumber='prescriptions',
+    getPrescriptionsByNumber='prescriptions/no_orden',
     getPrescriptionsByName='prescriptions/name',
     getPrescriptionsById='prescriptions/no_identificacion',
     getPrescriptionsByIps='prescriptions/ips',
