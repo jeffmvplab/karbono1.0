246a36ee39e2075ab9fd88ce11a8152c10c44683
danner01 <92951539+danner01@users.noreply.github.com>
2023-05-25 13:08:01 -0400

    www

--- src/views/auth/RegisterView/RegisterView.tsx
+++ src/views/auth/RegisterView/RegisterView.tsx
@@ -13,9 +13,10 @@ const RegisterView: React.FC<RegisterViewProps> = () => {
 
 	const {
 		errorEmail, messageErrorEmail,
-		errorPhone,messageErrorPhone,
+		errorPhone, messageErrorPhone,
 		errorPassword, messageErrorPassword,
-		errorPasswordConfirm,messageErrorPasswordConfirm
+		errorPasswordConfirm, messageErrorPasswordConfirm,
+		errorAuth,authOK
 
 	} = React.useContext(GlobalContext);
 
@@ -24,7 +25,7 @@ const RegisterView: React.FC<RegisterViewProps> = () => {
 		<>
 			<Stack spacing={2} sx={{ mb: 5, position: 'relative' }}>
 
-				<Typography sx={{fontSize:{xs:'24px',sm:'30px'}}} fontWeight={600}>
+				<Typography sx={{ fontSize: { xs: '24px', sm: '30px' } }} fontWeight={600}>
 					Empieza tu prueba
 					gratuita ahora
 				</Typography>
@@ -39,14 +40,17 @@ const RegisterView: React.FC<RegisterViewProps> = () => {
 
 			</Stack>
 
-			{
-				(errorEmail || errorPassword||errorPasswordConfirm||errorPhone)
-					? <Alert severity="warning" sx={{ mb: 3 ,borderRadius:'10px'}}>
-						{messageErrorEmail || messageErrorPassword||messageErrorPasswordConfirm||messageErrorPhone}
+			{(authOK)
+				? (errorEmail || errorPassword || errorPasswordConfirm || errorPhone)
+					? <Alert severity="warning" sx={{ mb: 3, borderRadius: '10px' }}>
+						{messageErrorEmail || messageErrorPassword || messageErrorPasswordConfirm || messageErrorPhone}
 					</Alert>
-					: <Alert severity="success" sx={{ mb: 3 ,borderRadius:'10px'}}>
+					: <Alert severity="success" sx={{ mb: 3, borderRadius: '10px' }}>
 						{'Sus datos tienen el formato correcto'}
 					</Alert>
+				: <Alert severity="error" sx={{ mb: 3, bgcolor: 'rgba(221,50,50,60%)', borderRadius: '10px' }}>
+					<Typography sx={{color:'white'}}>{errorAuth}</Typography> 
+				</Alert>
 			}
 
 			<AuthRegisterForm />
