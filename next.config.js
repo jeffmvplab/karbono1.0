d0f38a96ca87de54a70761e8df415aa214f2a9c7
danner01 <92951539+danner01@users.noreply.github.com>
2024-05-23 11:04:34 -0400

    ede

--- next.config.js
+++ next.config.js
@@ -9,14 +9,16 @@ const nextConfig = {
     headers: async() => {
         return [{
             source: "/(.*)",
-            headers: [{
+            headers: [
+
+                {
                     key: "X-Frame-Options",
                     value: "SAMEORIGIN",
                 },
-                {
-                    key: "Content-Security-Policy",
-                    value: "default-src 'self'; script-src 'self' https://trustedscripts.example.com; style-src 'self' https://trustedstyles.example.com; img-src 'self' data:; connect-src 'self' https://api.example.com; font-src 'self' https://fonts.gstatic.com; object-src 'none'; frame-ancestors 'self'; upgrade-insecure-requests;",
-                },
+                // {
+                //     key: "Content-Security-Policy",
+                //     value: "default-src 'self'; script-src 'self' https://trustedscripts.example.com; style-src 'self' https://trustedstyles.example.com; img-src 'self' data:; connect-src 'self' https://api.example.com; font-src 'self' https://fonts.gstatic.com; object-src 'none'; frame-ancestors 'self'; upgrade-insecure-requests;",
+                // },
                 {
                     key: "Access-Control-Allow-Origin",
                     value: "https://app.purelife.cloud/", // Cambia esto al dominio permitido
