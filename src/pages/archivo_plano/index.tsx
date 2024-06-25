f212566e58d83e94e815f1ea9b677aeee47398b5
danner01 <92951539+danner01@users.noreply.github.com>
2024-06-25 09:11:45 -0400

    cambio de nombre archivo plano

--- src/pages/archivo_plano/index.tsx
+++ src/pages/archivo_plano/index.tsx
@@ -1,17 +1,6 @@
 
 import { ReactElement, useContext } from 'react'
 import { NextPageWithLayout } from '@/pages/_app'
-import { ReportesProvider } from '@/views/ReportePrescripcion/context/ReportesProvider'
-import { GetServerSideProps } from 'next'
-import { MainLayout } from '@/layouts/MainLayout'
-import { convertirAPDF } from '@/utilities/view_pdf_convert'
-import PDFPrescriptionComponent from '@/views/ReportePrescripcion/components/PDFPrescriptionComponent'
-import { Stack, Avatar, Button } from '@mui/material'
-import { IoPrintOutline } from 'react-icons/io5'
-import style from 'styled-jsx/style'
-import CloseIcon from '@mui/icons-material/Close';
-import { ReportesContext } from '@/views/ReportePrescripcion/context/ReportesContext'
-import PDFView from '@/views/PDFView/PDFView'
 import { ArchivoPlanoView } from '@/views/ArchivoPlanoView'
 import PrescripcionLayout from '@/layouts/PrescripcionLayout/PrescripcionLayout'
 
